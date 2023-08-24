cb9be92bcc1bbfcdfd6e930d0f1432d1fd28b92f
poorvika <seamanand@gmail.com>
2023-08-24 15:48:21 +0530

    Challenge67 Assigment

---
+++ src/main/java/Week5/HomeWork/Challenge67_SortedUniqueArray.java
@@ -0,0 +1,31 @@
+package Week5.HomeWork;
+
+import java.util.Arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Challenge67_SortedUniqueArray {
+	@Test
+	public void test1() {
+		Assert.assertEquals(4, findSamllestValue(new int[] {1,4,3,2}));
+	}
+	
+	public int findSamllestValue(int[] value) {
+		int temp = 0;
+		Arrays.sort(value);
+		for(int i=0;i<=value.length;i+=2) {
+			//System.out.println(value[i]);
+			temp=value[i];
+			System.out.println(temp);
+			
+		}
+		
+		return temp;
+		
+		
+	
+		
+	}
+	
+}
